0eb1d96f2277a39b3364d4e5d11110c5071b6c0a
techai342 <tiktokusr4400@gmail.com>
2026-01-21 23:49:01 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav class="flex gap-6 p-4 justify-center border-b border-cyan-400">
+    <nav className="flex gap-6 p-4 justify-center border-b border-cyan-400">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/gallery">Gallery</Link>
       <Link to="/social">Social</Link>
+      <Link to="/admin" className="text-pink-400 font-bold">Admin Manager</Link>
     </nav>
   );
 }
