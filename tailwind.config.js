a6120b5daf6d95b931b9d75d7a66a08f294a8be1
techai342 <tiktokusr4400@gmail.com>
2026-01-22 01:03:03 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     extend: {
       colors: {
         neon: "#00ffff"
-      }
+      },
+      screens: {
+        'xs': '475px',
+        // Default screens already exist: sm:640, md:768, lg:1024, xl:1280, 2xl:1536
+      },
     }
   },
   plugins: []
