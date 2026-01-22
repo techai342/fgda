cc19773c36daf4ccbc9a539153c6a6e0d176def7
techai342 <tiktokusr4400@gmail.com>
2026-01-22 21:26:02 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    }
+  }
 })
