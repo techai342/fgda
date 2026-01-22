6e9d42a72e4f8b5af24bff1c0e1a455a521a20c4
techai342 <tiktokusr4400@gmail.com>
2026-01-22 22:57:48 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-[file name]: vite.config.js
-[file content begin]
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
@@ -9,28 +7,8 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Management Portal',
-        short_name: 'Management',
-        description: 'Tournament Management Control Panel',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: 'https://ik.imagekit.io/shaban/SHABAN-1769057701316_BszrYcha1.jpg',
-            sizes: '192x192',
-            type: 'image/jpg'
-          },
-          {
-            src: 'https://ik.imagekit.io/shaban/SHABAN-1769057701316_BszrYcha1.jpg',
-            sizes: '512x512',
-            type: 'image/jpg'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}']
-      }
+      includeAssets: ['favicon.ico'],
+      manifest: false // We'll use our own manifest.json
     })
   ],
   server: {
@@ -46,4 +24,3 @@ export default defineConfig({
     }
   }
 })
-[file content end]
