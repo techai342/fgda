48a77d1e9b1b95ad293aa62921f4bff1c31ebb7d
techai342 <tiktokusr4400@gmail.com>
2026-01-22 23:00:02 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico'],
-      manifest: false // We'll use our own manifest.json
-    })
-  ],
+  plugins: [react()],
   server: {
     historyApiFallback: true,
   },
